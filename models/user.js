8598ccc9960773d0d02f2b255969f355d5eacbaf
Puneet Dhankar <148076373+self-Puneet@users.noreply.github.com>
2025-09-21 15:47:31 +0530

    update user.js

--- models/user.js
+++ models/user.js
@@ -4,7 +4,7 @@ const  mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
 user_id: { type: String, required: true, unique: true },
 name: { type: String},
-email: { type: String, unique: true },
+email: { type: String },
 phone: { type: String, required: true, unique: true },
 roles: { type: [String], default: ['user'] },
 createdAt: { type: Date, default: Date.now }
