7b60dcd057dec8913aa129e39f43233ad610ca14
Naman Jain <namanjain@Namans-MacBook-Pro.local>
2025-09-21 15:15:59 +0530

    added base route

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/admin', adminRoutes);
-app.use('/user', userRoutes);
+app.use('/api/admin', adminRoutes);
+app.use('/api/user', userRoutes);
 
 module.exports = app;
