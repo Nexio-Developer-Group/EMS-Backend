fb29bbd62147e268c3b008a8b5fbbaf65a301d48
Naman Jain <namanjain@Namans-MacBook-Pro.local>
2025-09-21 15:49:57 +0530

    added spars

--- models/user.js
+++ models/user.js
@@ -1,14 +1,12 @@
-const  mongoose = require('mongoose');
-
+const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-user_id: { type: String, required: true, unique: true },
-name: { type: String},
-email: { type: String, unique: true },
-phone: { type: String, required: true, unique: true },
-roles: { type: [String], default: ['user'] },
-createdAt: { type: Date, default: Date.now }
+  user_id: { type: String, required: true, unique: true },
+  name: { type: String },
+  email: { type: String, unique: true, sparse: true }, // allow multiple nulls
+  phone: { type: String, required: true, unique: true },
+  roles: { type: [String], default: ['user'] },
+  createdAt: { type: Date, default: Date.now }
 });
-
 
 module.exports = mongoose.model('User', userSchema);
