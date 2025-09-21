56183b6a7f85970b0d3500840f007aac7aecaffa
Naman Jain <namanjain@Namans-MacBook-Pro.local>
2025-09-21 15:57:51 +0530

    added new repo stucutre

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -1,4 +1,5 @@
 // user controller
+const { data } = require('autoprefixer');
 const userService = require('../services/user');
 
 // user verify OTP controller
@@ -6,9 +7,20 @@ async function verifyOtp(req, res) {
   try {
     const { phone, otp } = req.body;
     const { user, token } = await userService.verifyOtp(phone, otp);
-    res.status(200).json({ msg: 'OTP verified successfully', user, token });
+    res.status(200).json({ 
+      status : 1,
+      data : {
+        token : token,
+        user : user
+      },
+      error : null,
+      message : "User verified successfully"});
   } catch (error) {
-    res.status(400).json({ error: error.message });
+    res.status(400).json({ 
+      status : 0,
+      data : null,
+      message : "User verification failed",
+      error: error.message });
   }
 }
 
@@ -17,9 +29,17 @@ async function login(req, res) {
   try {
     const { phone } = req.body;
     const { message } = await userService.loginUser(phone);
-    res.status(200).json({ msg: message });
+    res.status(200).json({ 
+      status : 1,
+      data : null,
+      error : null,
+      msg: message });
   } catch (error) {
-    res.status(400).json({ error: error.message });
+    res.status(400).json({ 
+      status : 0,
+      data : null,
+      message : "Login failed",
+      error: error.message });
   }
 }
 
